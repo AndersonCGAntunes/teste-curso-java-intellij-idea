9a6c5e869bd5da247c921a60eb9aaf36c70345ca
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2022-06-02 15:15:36 -0300

    Class Livro criada

--- teste-curso-java-intellij-idea/src/br/com/dio/PrimeiroPrograma.java
+++ teste-curso-java-intellij-idea/src/br/com/dio/PrimeiroPrograma.java
@@ -10,11 +10,49 @@ public class PrimeiroPrograma {
         gato.setCor("Laranja");
         gato.setIdade(3);
 
+        Livro livro = new Livro("O Xangô de Baker Street", 500);
+
 //        System.out.println(gato);
-        System.out.println(gato);
+//        System.out.println(gato);
+        System.out.println(livro);
 
         /*int a = 5;
         int b = 3;
         System.out.println("Hello, World" + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{\n" +
+                "nome='" + nome +
+                "'\nnumPaginas=" + numPaginas +
+                "\n}";
     }
 }
