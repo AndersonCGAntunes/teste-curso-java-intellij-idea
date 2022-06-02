5f8a45d9c0b7ea937d5f448df4494be344f0a088
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2022-06-02 14:58:01 -0300

    First commit

---
+++ teste-curso-java-intellij-idea/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        gato.setNome("Felícia");
+        gato.setCor("Laranja");
+        gato.setIdade(3);
+
+//        System.out.println(gato);
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello, World" + (a+b));*/
+    }
+}
